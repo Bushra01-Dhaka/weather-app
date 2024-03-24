f2ed733bf2deab08cc20300dfe56359df27db82e
Bushra01-Dhaka <humayraanjum87@gmail.com>
2024-03-24 16:08:54 +0600

    using usestate dynamically added weather icon by fetching from api

--- src/Pages/Search/Search.jsx
+++ src/Pages/Search/Search.jsx
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom";
 import search_img from "../../assets/images/solid.png";
-import { FaCloudRain, FaSearch, FaWind } from "react-icons/fa";
+import { FaSearch, FaWind } from "react-icons/fa";
 import Aos from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { MdWaves } from "react-icons/md";
+import clear_icon from "../../assets/images/Assets/Assets/rain.png";
+import cloud_icon from "../../assets/images/Assets/Assets/cloud.png";
+import drizzle_icon from "../../assets/images/Assets/Assets/drizzle.png";
+import rain_icon from "../../assets/images/Assets/Assets/rain.png";
+import snow_icon from "../../assets/images/Assets/Assets/snow.png";
 
 const Search = () => {
   useEffect(() => {
@@ -12,6 +17,9 @@ const Search = () => {
   }, []);
 
   let api_key = "66e0be18fa493f93e7ba93ce26949624";
+
+  const [wicon, setWicon] = useState(clear_icon);
+
   const search = async () => {
     const element = document.getElementsByClassName("locationName");
     const searchLocation = element[0].value;
@@ -34,8 +42,44 @@ const Search = () => {
 
     humidity[0].innerHTML = data.main.humidity + "%";
     wind[0].innerHTML = data.wind.speed + "km/h";
-    temperature[0].innerHTML = data.main.temp + "°C";
+    temperature[0].innerHTML = Math.floor(data.main.temp) + "°C";
     location[0].innerHTML = data.name;
+
+    if(data.weather[0].icon === "01d" || data.weather[0].icon === "01n")
+    {
+      setWicon(clear_icon);
+    }
+    else if(data.weather[0].icon === "02d" || data.weather[0].icon === "02n")
+    {
+      setWicon(cloud_icon);
+    }
+    else if(data.weather[0].icon === "03d" || data.weather[0].icon === "03n")
+    {
+      setWicon(drizzle_icon);
+    }
+    else if(data.weather[0].icon === "04d" || data.weather[0].icon === "04n")
+    {
+      setWicon(drizzle_icon);
+    }
+    else if(data.weather[0].icon === "09d" || data.weather[0].icon === "09n")
+    {
+      setWicon(rain_icon);
+    }
+    else if(data.weather[0].icon === "10d" || data.weather[0].icon === "10n")
+    {
+      setWicon(rain_icon);
+    }
+    else if(data.weather[0].icon === "13d" || data.weather[0].icon === "13n")
+    {
+      setWicon(snow_icon);
+    }
+    else
+    {
+      setWicon(clear_icon);
+    }
+
+
+
 
   };
 
@@ -78,12 +122,14 @@ const Search = () => {
               </Link>
             </div>
           </div>
+          
 
           <div>
             <div className="flex justify-center">
-              <FaCloudRain className="text-8xl my-6 text-center"></FaCloudRain>
+              {/* <FaCloudRain className="text-8xl my-6 text-center"></FaCloudRain> */}
+              <img src={wicon} alt="" />
             </div>
-            <h1 className="text-center text-6xl text-white weather-temp">
+            <h1 className="text-center text-6xl text-white weather-temp py-2">
               29{" "}
               <sup>
                 {" "}
